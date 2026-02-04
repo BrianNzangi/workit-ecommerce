b11091941fa55d6e267765eb2acace3da5a2b3e9
Brian Nzangi <briankioko.bn@gmail.com>
2026-02-04 22:25:55 +0300

    feat: Implement Drizzle ORM for database connectivity and add `SKIP_DB_CHECK` environment variable to Dockerfile.

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === "production" && !process.env.SKIP_DB_CHECK) {
         throw new Error("DATABASE_URL is not defined");
     } else {
         console.warn("⚠️ DATABASE_URL is not defined, using default local postgres");
